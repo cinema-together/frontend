aefc3fb1d5d0a6e69ece2ad28b8fc1c394e51fa0
Alexander Erdyakov <sa@prg.re>
2022-05-04 01:50:46 +0300

    добавляет авторизацию

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,123 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Disclosure as="nav" class="bg-neutral-900" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex">
+          <div class="-ml-2 mr-2 flex items-center md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span class="sr-only">Open main menu</span>
+              <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+          <div class="flex-shrink-0 flex items-center">
+            <img class="block lg:hidden h-8 w-auto" src="./assets/logo.svg" alt="Кино вместе" />
+            <img class="hidden lg:block h-8 w-auto" src="./assets/logo.svg" alt="Кино вместе" />
+          </div>
+        </div>
+        <div class="flex items-center" v-if="isAuthenticated">
+          <div class="flex-shrink-0">
+            <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-orange-500">
+              <span>Создать комнату</span>
+            </button>
+          </div>
+          <div class="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
+            <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            <!-- Profile dropdown -->
+            <Menu as="div" class="ml-3 relative">
+              <div>
+                <MenuButton class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span class="sr-only">Open user menu</span>
+                  <img class="h-8 w-8 rounded-full" :src="user.picture" alt="" />
+                </MenuButton>
+              </div>
+              <transition enter-active-class="transition ease-out duration-200" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <MenuItem v-slot="{ active }">
+                    <a href="#" @click.prevent="logout" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign out</a>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+        </div>
+        <div class="flex items-center" v-if="!isAuthenticated">
+            <button type="button" @click="login" class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-orange-500">
+              <span>Войти / Зарегистрироваться</span>
+            </button>
+          </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="md:hidden" v-if="isAuthenticated">
+      <div class="pt-4 pb-3 border-t border-gray-700">
+        <div class="flex items-center px-5 sm:px-6">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" :src="user.picture" alt="" />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium text-white">{{ user.name }}</div>
+            <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
+          </div>
+          <button type="button" class="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-3 px-2 space-y-1 sm:px-3">
+          <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">{{ item.name }}</DisclosureButton>
+        </div>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+import { useAuth0 } from '@auth0/auth0-vue';
+
+
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    BellIcon,
+    MenuIcon,
+    XIcon,
+  },
+  setup() {
+    const { isAuthenticated, isLoading, user, loginWithRedirect, logout } = useAuth0();
+    return {
+      isAuthenticated,
+      isLoading,
+      userNavigation,
+      user,
+      login: () => {
+        loginWithRedirect();
+      },
+      logout: () => {
+        logout({
+          returnTo: window.location.origin
+        });
+      }
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createAuth0 } from "@auth0/auth0-vue";
+import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+
+app
+    .use(
+        createAuth0({
+          domain: process.env.VUE_APP_AUTH0_DOMAIN,
+          client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
+          redirect_uri: window.location.origin,
+        })
+      )
+    .mount('#app')
